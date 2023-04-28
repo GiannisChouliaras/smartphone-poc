59b9f273c2e0b8a8858a01d22751bd27c49345ee
Ioannis Chouliaras <giannis.chouliaras10@gmail.com>
2023-04-28 10:31:30 +0300

    First commit

---
+++ src/main/java/com/vodafone/smartphonemigrationpoc/SmartphoneMigrationPocApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.smartphonemigrationpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartphoneMigrationPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartphoneMigrationPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vodafone/smartphonemigrationpoc/SmartphoneMigrationPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vodafone.smartphonemigrationpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartphoneMigrationPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
