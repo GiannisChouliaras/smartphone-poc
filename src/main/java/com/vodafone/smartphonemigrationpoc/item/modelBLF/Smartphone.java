a5dd43aadc1adb79e92d92be6b337b03f5160292
ioannis-chouliaras <ioannis.chouliaras@vodafone.com>
2023-05-04 15:04:08 +0300

    UPDATE: classes with annotations

--- src/main/java/com/vodafone/smartphonemigrationpoc/item/modelBLF/Smartphone.java
+++ src/main/java/com/vodafone/smartphonemigrationpoc/item/modelBLF/Smartphone.java
@@ -1,5 +1,12 @@
 package com.vodafone.smartphonemigrationpoc.item.modelBLF;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Smartphone {
     private boolean talkPhone;
     private boolean ecoRating;
--- src/main/java/com/vodafone/smartphonemigrationpoc/item/modelBLF/TangibleCharacteristic.java
+++ src/main/java/com/vodafone/smartphonemigrationpoc/item/modelBLF/TangibleCharacteristic.java
@@ -1,5 +1,12 @@
 package com.vodafone.smartphonemigrationpoc.item.modelBLF;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TangibleCharacteristic {
     private boolean wifi;
     private Device device;
